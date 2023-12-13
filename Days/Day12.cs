181d5bdb54e768a3787b1a19abf4e6406e565902
Lukas Wegmüller <lukas.wegmueller@fmh.ch>
2023-12-13 12:48:11 +0100

    Day 12 (1)

---
+++ Days/Day12.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using AdventOfCode2023.Days.Interfaces;
+
+namespace AdventOfCode2023.Days;
+
+public class Day12 : IRiddle
+{
+    public string SolveFirst()
+    {
+        var input = File.ReadAllLines("Days\\Inputs\\Day12.txt");
+
+        var sum = input
+            .Select(line => line.Split(' '))
+            .Select(splits =>
+                CalculatePossibilities(
+                    splits[0].Trim(),
+                    splits[1].Trim().Split(",").Select(int.Parse)))
+            .Sum();
+
+        return sum.ToString();
+    }
+
+    public string SolveSecond()
+    {
+        var input = File.ReadAllLines("Days\\Inputs\\Day12.txt");
+
+        var sum = 0;
+
+        var splitsList = input.Select(line => line.Split(' ')).ToList();
+        for (var i = 0; i < splitsList.Count; i++)
+        {
+            var splits = splitsList[i];
+            sum += CalculatePossibilities(
+                splits[0].Trim(),
+                splits[1].Trim().Split(",").Select(int.Parse),
+                4);
+            Console.WriteLine($"Solved {i + 1} of {splitsList.Count} ({(i + 1) * 100 / (double)splitsList.Count:0.##}%)");
+        }
+
+        return sum.ToString();
+    }
+
+    private static int CalculatePossibilities(string input, IEnumerable<int> numbers, int repeat = 0)
+    {
+        var text = input;
+        var numbersList = numbers.Reverse().ToList();
+        var stack = new Stack<int>(numbersList);
+
+        for (var i = 0; i < repeat; i++)
+        {
+            text += "?" + input;
+
+            foreach (var item in numbersList)
+            {
+                stack.Push(item);
+            }
+        }
+
+        return Solve(text.ToCharArray(), stack);
+    }
+
+    private static int Solve(char[] input, Stack<int> numbers)
+    {
+        return Solve(input, numbers, numbers.Pop(), true);
+    }
+
+    private static int Solve(char[] input, Stack<int> numbers, int remaining, bool fromPoint = false)
+    {
+        var c = input[0];
+
+        if (input.Length == 1)
+        {
+            return !numbers.Any() && (remaining == 0 && c is '?' or '.' || remaining == 1 && c is '?' or '#') ? 1 : 0;
+        }
+
+        switch (c)
+        {
+            case '#':
+                if (remaining == 0)
+                {
+                    return 0;
+                }
+
+                var newStack1 = numbers.CloneStack();
+                return Solve(input[1..], newStack1, remaining - 1);
+
+            case '.':
+                if (fromPoint)
+                {
+                    var newStack2 = numbers.CloneStack();
+                    return Solve(input[1..], newStack2, remaining, true);
+                }
+
+                if (remaining > 0)
+                {
+                    return 0;
+                }
+
+                if (!numbers.Any())
+                {
+                    var newStack3 = numbers.CloneStack();
+                    return Solve(input[1..], newStack3, remaining, true);
+                }
+
+                var newStack4 = numbers.CloneStack();
+                return Solve(input[1..], newStack4, newStack4.Pop(), true);
+            case '?':
+                var a = 0;
+
+                if (remaining > 0)
+                {
+                    var newStack5 = numbers.CloneStack();
+                    a = Solve(input[1..], newStack5, remaining - 1);
+                }
+
+                if (fromPoint)
+                {
+                    var newStack6 = numbers.CloneStack();
+                    return a + Solve(input[1..], newStack6, remaining, true);
+                }
+
+                if (remaining > 0)
+                {
+                    return a;
+                }
+
+                if (!numbers.Any())
+                {
+                    var newStack7 = numbers.CloneStack();
+                    return Solve(input[1..], newStack7, remaining, true);
+                }
+
+                var newStack8 = numbers.CloneStack();
+                return Solve(input[1..], newStack8, newStack8.Pop(), true);
+        }
+
+        throw new Exception("Should not happen");
+    }
+
+    private static bool IsPossbile(string input, IEnumerable<int> numbers)
+    {
+        return input.Split(".").Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Length).SequenceEqual(numbers);
+    }
+}
+
+
+public static class StackExtensions
+{
+    public static Stack<T> CloneStack<T>(this Stack<T> original)
+    {
+        var arr = new T[original.Count];
+        original.CopyTo(arr, 0);
+        Array.Reverse(arr);
+        return new Stack<T>(arr);
+    }
+}
