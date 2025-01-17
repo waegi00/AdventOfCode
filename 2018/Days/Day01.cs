a80e08cc41c835a65cf9b317e7001c6f108b9121
Lukas Wegmüller <31287504+waegi00@users.noreply.github.com>
2025-01-17 14:28:01 +0100

    2018 Day 01

---
+++ 2018/Days/Day01.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Interfaces;
+using AdventOfCode.Library.Input;
+
+namespace AdventOfCode._2018.Days;
+
+public class Day01 : IRiddle
+{
+    public string SolveFirst()
+    {
+        return this.InputToLines()
+            .Sum(int.Parse)
+            .ToString();
+    }
+
+    public string SolveSecond()
+    {
+        var input = this.InputToLines()
+            .Select(int.Parse)
+            .ToArray();
+
+        var seen = new HashSet<int>();
+        var i = 0;
+        var curr = 0;
+
+        do
+        {
+            curr += input[i++ % input.Length];
+        } while (seen.Add(curr));
+
+        return curr.ToString();
+    }
+}
