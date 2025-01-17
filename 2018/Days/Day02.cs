549dbae877aa4c41754552d1dc033afdef282d8c
Lukas Wegmüller <31287504+waegi00@users.noreply.github.com>
2025-01-17 16:33:57 +0100

    2018 Days 02 - 03

---
+++ 2018/Days/Day02.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode.Interfaces;
+using AdventOfCode.Library.Input;
+
+namespace AdventOfCode._2018.Days;
+
+public class Day02 : IRiddle
+{
+    public string SolveFirst()
+    {
+        var input = this.InputToLines();
+
+        var twos = 0;
+        var threes = 0;
+
+        foreach (var line in input)
+        {
+            var f = line
+                .GroupBy(x => x)
+                .Select(x => x.Count())
+                .ToArray();
+
+            if (f.Any(x => x == 2))
+            {
+                twos++;
+            }
+
+            if (f.Any(x => x == 3))
+            {
+                threes++;
+            }
+        }
+
+        return (twos * threes).ToString();
+    }
+
+    public string SolveSecond()
+    {
+        var input = this.InputToLines();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var pair = input
+                .Select(x => x.Remove(i, 1))
+                .GroupBy(x => x)
+                .FirstOrDefault(x => x.Count() > 1);
+
+            if (pair != null)
+            {
+                return pair.First();
+            }
+        }
+
+        return "";
+    }
+}
---
+++ 2018/Days/Day03.cs
@@ -0,0 +1,78 @@
+﻿using AdventOfCode.Interfaces;
+using AdventOfCode.Library.Array;
+using AdventOfCode.Library.Input;
+
+namespace AdventOfCode._2018.Days;
+
+public class Day03 : IRiddle
+{
+    public string SolveFirst()
+    {
+        var input = this.InputToLines()
+            .Select(x => x.Split(' '))
+            .ToArray();
+
+        const int size = 1000;
+        var grid = new int[size][];
+        foreach (var i in Enumerable.Range(0, size))
+        {
+            grid[i] = new int[size];
+        }
+
+        foreach (var line in input)
+        {
+            var pos = line[2][..^1].Split(',').Select(int.Parse).ToArray();
+            var area = line[^1].Split('x').Select(int.Parse).ToArray();
+            grid.SetValues(pos[0], pos[1], pos[0] + area[0] - 1, pos[1] + area[1] - 1, v => v + 1);
+        }
+
+        return grid.Sum(x => x.Count(y => y > 1)).ToString();
+    }
+
+    public string SolveSecond()
+    {
+        var input = this.InputToLines()
+            .Select(x => x.Split(' '))
+            .ToArray();
+
+        const int size = 1000;
+        var grid = new int[size][];
+        foreach (var i in Enumerable.Range(0, size))
+        {
+            grid[i] = new int[size];
+        }
+
+        foreach (var line in input)
+        {
+            var pos = line[2][..^1].Split(',').Select(int.Parse).ToArray();
+            var area = line[^1].Split('x').Select(int.Parse).ToArray();
+            grid.SetValues(pos[0], pos[1], pos[0] + area[0] - 1, pos[1] + area[1] - 1, v => v + 1);
+        }
+
+        foreach (var line in input)
+        {
+            var id = int.Parse(line[0][1..]);
+            var pos = line[2][..^1].Split(',').Select(int.Parse).ToArray();
+            var area = line[^1].Split('x').Select(int.Parse).ToArray();
+
+            var failed = false;
+            for (var i = pos[0]; i < pos[0] + area[0] && !failed; i++)
+            {
+                for (var j = pos[1]; j < pos[1] + area[1] && !failed; j++)
+                {
+                    if (grid[i][j] != 1)
+                    {
+                        failed = true;
+                    }
+                }
+            }
+
+            if (!failed)
+            {
+                return id.ToString();
+            }
+        }
+
+        return "";
+    }
+}
